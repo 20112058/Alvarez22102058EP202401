726ad40d70df5691b2a8ec71d9aecac62ab43c8a
Daniel Alvarez <22102048@ue.edu.pe>
2024-05-19 12:30:33 -0500

    1er commit

--- app/src/main/java/com/example/alvarez22102058ep202401/MenuActivity.kt
+++ app/src/main/java/com/example/alvarez22102058ep202401/MenuActivity.kt
@@ -8,23 +8,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btStarted: Button = findViewById(R.id.btStarted)
 
-        btStarted.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+        val btPrizes: Button = findViewById(R.id.btPrizes)
+        val btTeams: Button = findViewById(R.id.btTeams)
+        val btStadium: Button = findViewById(R.id.btStadium)
+
+        btPrizes.setOnClickListener {
+            val intent = Intent(this, PrizesActivity::class.java)
             startActivity(intent)
 
         }
 
+        btTeams.setOnClickListener {
+            val intent = Intent(this, TeamsActivity::class.java)
+            startActivity(intent)
+
+        }
+        btStadium.setOnClickListener {
+            val intent = Intent(this, StadiumsActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/alvarez22102058ep202401/TeamsActivity.kt
+++ app/src/main/java/com/example/alvarez22102058ep202401/TeamsActivity.kt
@@ -1,30 +1,20 @@
 package com.example.alvarez22102058ep202401
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TeamsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_teams)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btStarted: Button = findViewById(R.id.btStarted)
-
-        btStarted.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-
-        }
-
     }
 }
